b05762f85beda48d8278dd98163259c499ad6ff5
Evan Astle <starphys@gmail.com>
2024-04-14 12:02:13 -0700

    Further cosmetic improvements

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -66,11 +66,12 @@ function Chat () {
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
-      width: '100vw'
+      width: '100vw',
+      background: 'linear-gradient(23deg, hsl(49deg, 100%, 69%) 0%, hsl(16deg, 80%, 61%) 2%, hsl(330deg, 81%, 34%) 12%, hsl(259deg, 100%, 15%) 50%, hsl(212deg, 100%, 25%) 88%, hsl(197deg, 100%, 30%) 98%, hsl(183deg, 79%, 36%) 100%)'
     }}
     >
       <Box sx={{
-        width: 371,
+        width: 800,
         backgroundColor: 'lightgrey',
         borderRadius: 2,
         overflow: 'hidden',
@@ -78,24 +79,34 @@ function Chat () {
       }}
       >
         <Box sx={{
-          width: 370,
+          width: 800,
           height: 40,
           backgroundColor: '#404040',
           display: 'flex',
-          justifyContent: 'center',
-          alignContent: 'center',
-          paddingTop: 2
+          justifyContent: 'space-between',
+          alignItems: 'center'
         }}
         >
-          <Typography color='white'>Food Security and Nutrition GPT</Typography>
+          <Box sx={{ width: 'auto', paddingTop: 0.5 }}>
+            <img
+              src='https://tmpfiles.nohat.cc/visualhunter-a1bf6d3746.png'
+              width='45'
+              height='45'
+            />
+          </Box>
+          <Typography color='white' sx={{ flexGrow: 0 }}>Food Security and Nutrition GPT</Typography>
+          <div style={{ width: '45px' }} />
         </Box>
         <Box
           ref={scrollRef}
           sx={{
-            width: 370,
-            height: 400,
+            width: 800,
+            height: 600,
             overflow: 'auto',
-            backgroundColor: 'lightgrey'
+            backgroundColor: 'lightgrey',
+            overflow: 'overlay',
+            scrollbarWidth: 'thin',
+            scrollbarColor: 'rgba(0, 0, 0, 0.5) transparent'
           }}
         >
           <Box sx={{ display: 'flex', flexDirection: 'column', padding: 1 }}>
@@ -103,8 +114,8 @@ function Chat () {
               <Box
                 key={index}
                 sx={{
-                  backgroundColor: message.role === 'user' ? 'white' : 'blue',
-                  color: message.role === 'user' ? 'black' : 'white',
+                  backgroundColor: message.role === 'user' ? 'blue' : 'white',
+                  color: message.role === 'user' ? 'white' : 'black',
                   padding: 1,
                   borderRadius: 1,
                   marginBottom: 1,
@@ -134,7 +145,7 @@ function Chat () {
             placeholder='Type your message...'
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            sx={{ width: 300, marginRight: 1 }}
+            sx={{ width: 800, marginRight: 1 }}
           />
           <Button variant='contained' color='primary' type='submit'>
             <SendIcon />
