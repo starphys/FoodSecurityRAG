d1e632c83d7c6986647a04c214ea2ba93414a8c8
Evan Astle <starphys@gmail.com>
2024-04-13 21:16:11 -0700

    Call RAG from app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
-import json
-
 from flask import Flask, request, jsonify
+from RAG import RAG
 
 app = Flask(__name__)
-
+rag = RAG("SOFI-2023.txt")
 
 @app.route('/')
 def hello_world():
@@ -14,7 +13,9 @@ def hello_world():
 def message():
     message_history = request.json['message_history']
 
+    rag.generate_answer(messages=message_history)
+
     return jsonify(response=f"LLM response to [{message_history[-1]['content']}] here")
 
 
-app.run(debug=True)
+app.run()
